a8fd29f2094862172ad8dfcbaccd3709eeb6612f
DawonChoi <41173953+DawonChoi@users.noreply.github.com>
2020-06-10 17:17:37 +0900

    update view : return -> provide static files

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
 def index():
-    return "Hello world"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return "All about Flask!"
+    return """
+    <h1 style='color: red;'>I'm a red H1 heading!</h1>
+    <p>This is a lovely little paragraph</p>
+    <code>Flask is <em>awesome</em></code>
+    """
