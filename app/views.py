2505ae968e58ab66288451cd94e97d1c0f479050
DawonChoi <41173953+DawonChoi@users.noreply.github.com>
2020-06-29 14:24:38 +0900

    Jinja Designing and test some jinja function

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+from datetime import datetime
 
 from flask import render_template
 
@@ -13,3 +14,64 @@ def about():
     <p>This is a lovely little paragraph</p>
     <code>Flask is <em>awesome</em></code>
     """
+
+@app.template_filter("clean_date")
+def clean_date(dt):
+    return dt.strftime("%d %b %Y")
+
+@app.route("/jinja")
+def jinja():
+    # Strings
+    my_name = "Dawon"
+
+    # Integers
+    my_age = 18
+
+    # Lists
+    langs = ["Python", "JavaScript", "Bash", "Ruby", "C", "Rust"]
+
+    # Dictionaries
+    friends = {
+        "Tony": 43,
+        "Cody": 28,
+        "Amy": 26,
+        "Clarissa": 23,
+        "Wendell": 39
+    }
+
+    # Tuples
+    colors = ("Red", "Blue")
+
+    # Booleans
+    cool = True
+
+    # Classes
+    class GitRemote:
+        def __init__(self, name, description, domain):
+            self.name = name
+            self.description = description 
+            self.domain = domain
+
+        def pull(self):
+            return f"Pulling repo '{self.name}'"
+
+        def clone(self, repo):
+            return f"Cloning into {repo}"
+
+    my_remote = GitRemote(
+        name="flask-Hug",
+        description="flask web server that provide file uploading but main offering is .mp3",
+        domain="https://github.com/DawonChoi/Flask-Hug.git"
+    )
+
+    # Functions
+    def repeat(x, qty=1):
+        return x * qty
+
+    date = datetime.utcnow()
+
+    return render_template(
+        "public/jinja.html", my_name=my_name, my_age=my_age, langs=langs,
+        friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
+        my_remote=my_remote, repeat=repeat, date=date
+    )
